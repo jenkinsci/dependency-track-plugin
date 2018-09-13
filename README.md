68d30b39c8fc4eca4d3c68554ff0261deee9b4c9
Steve Springett <steve@springett.us>
2018-09-13 12:04:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+[![License][license-image]][license-url]
+[![Website](https://img.shields.io/badge/https://-dependencytrack.org-blue.svg)](https://dependencytrack.org/)
+[![Documentation](https://img.shields.io/badge/read-documentation-blue.svg)](https://docs.dependencytrack.org/)
+
+
+Dependency-Track Jenkins Plugin
+==============================
+
+Dependency-Track is an intelligent Software Composition Analysis (SCA) platform that allows organizations to identify 
+and reduce risk from the use of third-party and open source components.
+
+The Dependency-Track Jenkins plugin aids in publishing [CycloneDX](https://cyclonedx.org/) and [SPDX](https://spdx.org/) 
+BoMs as well as Dependency-Check XML reports to the Dependency-Track platform.
+
+Copyright & License
+-------------------
+
+Dependency-Track and the Dependency-Track Jenkins Plugin is Copyright (c) Steve Springett. All Rights Reserved.
+
+Permission to modify and redistribute is granted under the terms of the Apache 2.0 license.
+
+  [wiki]: https://wiki.jenkins-ci.org/display/JENKINS/OWASP+Dependency-Track+Plugin
+  [license-image]: https://img.shields.io/badge/license-apache%20v2-brightgreen.svg
+  [license-url]: https://github.com/DependencyTrack/jenkins-plugin/blob/master/LICENSE.txt
---
+++ src/main/java/org/jenkinsci/plugins/DependencyTrack/PluginUtil.java
@@ -0,0 +1,105 @@
+/*
+ * This file is part of Dependency-Track Jenkins plugin.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jenkinsci.plugins.DependencyTrack;
+
+import hudson.FilePath;
+import hudson.model.AbstractBuild;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.util.FormValidation;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.stapler.QueryParameter;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class PluginUtil implements Serializable {
+
+    private static final long serialVersionUID = 5780698407191725723L;
+
+    private PluginUtil() { }
+
+    /**
+     * Replace a Jenkins environment variable in the form ${name} contained in the
+     * specified String with the value of the matching environment variable.
+     */
+    static String substituteVariable(final Run<?, ?> build, final TaskListener listener, final String parameterizedValue) {
+        // We cannot perform variable substitution for Pipeline jobs, so check to see if Run is an instance
+        // of AbstractBuild or not. If not, simply return the value without attempting variable substitution.
+        if (! (build instanceof AbstractBuild)) {
+            return parameterizedValue;
+        }
+        try {
+            if (parameterizedValue != null && parameterizedValue.contains("${")) {
+                final int start = parameterizedValue.indexOf("${");
+                final int end = parameterizedValue.indexOf("}", start);
+                final String parameter = parameterizedValue.substring(start + 2, end);
+                final String value = build.getEnvironment(listener).get(parameter);
+                if (value == null) {
+                    throw new IllegalStateException(parameter);
+                }
+                final String substitutedValue = parameterizedValue.substring(0, start) + value + (parameterizedValue.length() > end + 1 ? parameterizedValue.substring(end + 1) : "");
+                if (end > 0) { // recursively substitute variables
+                    return substituteVariable(build, listener, substitutedValue);
+                } else {
+                    return parameterizedValue;
+                }
+            } else {
+                return parameterizedValue;
+            }
+        } catch (IOException | InterruptedException e) {
+            return parameterizedValue;
+        }
+    }
+
+    /**
+     * Performs input validation when submitting the global config
+     * @param value The value of the URL as specified in the global config
+     * @return a FormValidation object
+     */
+    static FormValidation doCheckUrl(@QueryParameter String value) {
+        if (StringUtils.isBlank(value)) {
+            return FormValidation.ok();
+        }
+        try {
+            new URL(value);
+        } catch (MalformedURLException e) {
+            return FormValidation.error("The specified value is not a valid URL");
+        }
+        return FormValidation.ok();
+    }
+
+    /**
+     * Performs input validation when submitting the global config
+     * @param value The value of the path as specified in the global config
+     * @return a FormValidation object
+     */
+    static FormValidation doCheckPath(@QueryParameter String value) {
+        if (StringUtils.isBlank(value)) {
+            return FormValidation.ok();
+        }
+        try {
+            final FilePath filePath = new FilePath(new File(value));
+            filePath.exists();
+        } catch (Exception e) {
+            return FormValidation.error("The specified value is not a valid path");
+        }
+        return FormValidation.ok();
+    }
+
+}
